21e68e28be420716deccc1df2b0c81a39bfb5181
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-09 15:18:19 +0330

    pattern

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function checked_email(){
     let form=document.getElementById('form');
     let email=document.getElementById('email').value;
     let text=document.getElementById('text');
-   
+    
+    let pattern_email=/^[^]+@[^]\.[a-z]{2,3}$/;
     
 }
