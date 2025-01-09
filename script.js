d3b999ba310b26895b27deed2884de45d7dad55e
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-09 15:51:40 +0330

    js

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ function checked_email(){
     let pattern_email=/^[^]+@[^]+\.[a-z]{2,3}$/;
     if(email.match(pattern_email)){
         form.classList.add('valid');
+        form.classList.remove('invalid');
+        text.innerHTML="ایمیل شما معتبر است";
+        text.style.color='#01ff01';
+    }else{
+        form.classList.add('invalid');
+        form.classList.remove('valid');
+        text.innerHTML="ایمیل شما معتبر نیست";
+        text.style.color='red';
     }
 }
