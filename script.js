32f5ffc733e7ff2efdf539e46292222efd90e744
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-09 15:04:15 +0330

    start js

---
+++ script.js
@@ -0,0 +1,7 @@
+function checked_email(){
+    let form=document.getElementById('form');
+    let email=document.getElementById('email').value;
+    let text=document.getElementById('text');
+   
+    
+}
